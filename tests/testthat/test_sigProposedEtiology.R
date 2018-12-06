bb939b6724487877d76b9f95faada7065e19518f
Xindi Zhang <xindi.zhang@mail.utoronto.ca>
2018-12-06 00:17:25 -0500

    Fixed a little problem in testthat

--- tests/testthat/test_sigProposedEtiology.R
+++ tests/testthat/test_sigProposedEtiology.R
@@ -1,24 +1,17 @@
 #test_lseq.R
 
 context("setUp")
-
-# ==== BEGIN SETUP AND PREPARE =================================================
-#
-data_path <- system.file("extdata", "mutational_signatures.txt", package = "SigAct")
-
 #
 # ==== END SETUP AND PREPARE ===================================================
 # 
 test_that("Mutational signature infomation  ",{
-  mutationalSig <- readSignatures(data_path)
-  actual_info <- sigProposedEtiology("SBS36", mutationalSig)
-  expected_info <- "Defective base excision repair, including DNA damage due to reactive oxygen species, due to biallelic germline or somatic MUTYH mutations."
+  actual_info <- sigProposedEtiology("S36")
+  expected_info <- "S36: Defective base excision repair, including DNA damage due to reactive oxygen species, due to biallelic germline or somatic MUTYH mutations."
   expect_equal(actual_info, expected_info)
 })
 
 test_that("Mutational signature infomation  ",{
-  mutationalSig <- readSignatures(data_path)
-  actual_info <- sigProposedEtiology("36", mutationalSig)
+  actual_info <- sigProposedEtiology("36")
   expected_info <- "Invalid Signature name"
   expect_equal(actual_info, expected_info)
 })
