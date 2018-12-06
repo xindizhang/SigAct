f775dd4f49451d17653bee510b7c3825eca84f97
Xindi Zhang <xindi.zhang@mail.utoronto.ca>
2018-12-05 23:53:04 -0500

    update

--- R/runSinApp.R
+++ R/runSinApp.R
@@ -1,5 +1,5 @@
 # runSinApp.R
-
+# 
 #' \code{runRptApp} launch the shiny app distributed with this package framework
 #'
 #' \code{runRptApp} launches the shiny app for which the code has been placed in  \code{./inst/shiny-scripts/rptApp/}.
